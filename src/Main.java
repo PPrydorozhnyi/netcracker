86b8678293b37d2ceb4b34e82c2066dbbf0adcfd
Peter Pridorozhny <Peter35128@gmail.com>
2017-11-20 21:45:08 +0200

    find by last name

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import dao.EmployeeDAO;
 import objects.Employee;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Created by drake on 19/11/17.
@@ -11,8 +12,26 @@ public class Main {
     public static void main(String[] args) {
         EmployeeDAO dao = new EmployeeDAO();
         Employee emp = null;
+        ArrayList<Employee> employees = null;
         try {
             emp = dao.getByID(1511094532520L);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            employees = dao.getByLastName("WARD");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (employees != null) {
+            for (Employee empl : employees)
+                System.out.println(empl);
+        }
+
+        try {
+            dao.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/dao/EmployeeDAO.java
+++ src/dao/EmployeeDAO.java
@@ -3,6 +3,7 @@ package dao;
 import objects.Employee;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * @author P.Pridorozhny
@@ -101,6 +102,80 @@ public class EmployeeDAO {
 
         return employee;
     }
+
+    public ArrayList<Employee> getByLastName(String lastName) throws SQLException {
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        Employee employee = null;
+        ResultSet rs = null;
+        PreparedStatement myStmt = null;
+
+        myStmt = myConn.prepareStatement("select objects.NAME nam, fi_name.TEXT_VALUE fname, " +
+                "job.TEXT_VALUE jobb, hiredate.DATE_VALUE hiredatee, sal.NUMBER_VALUE sall, " +
+                "comm.NUMBER_VALUE commm, deptno.NUMBER_VALUE deptnoo " +
+                "from objects join object_types on " +
+                "object_types.object_type_id = objects.object_type_id " +
+                "join attr on attr.object_type_id = object_types.object_type_id " +
+                "and attr.name='ENAME' " +
+                "join params fi_name on fi_name.attr_id = attr.attr_id and " +
+                "fi_name.object_id = objects.object_id " +
+                "join attr job_attr on " +
+                "job_attr.object_type_id = Object_types.object_type_id and " +
+                "job_attr.name='JOB'\n" +
+                "join Params job on job.attr_id=job_attr.attr_id" +
+                " and job.object_id=Objects.object_id" +
+                " join attr hiredate_attr on " +
+                "hiredate_attr.object_type_id=object_types.object_type_id and " +
+                "hiredate_attr.name='HIREDATE'" +
+                "join params hiredate on hiredate.attr_id=hiredate_attr.attr_id and " +
+                "hiredate.object_id=objects.object_id" +
+                " join attr sal_attr on " +
+                "sal_attr.object_type_id=object_types.object_type_id and " +
+                "sal_attr.name='SAL'" +
+                "join Params sal on sal.attr_id=sal_attr.attr_id and " +
+                "sal.object_id=objects.object_id" +
+                " join attr comm_attr on " +
+                "comm_attr.object_type_id=Object_types.object_type_id and " +
+                "comm_attr.name='COMM'" +
+                "join Params comm on comm.attr_id=comm_attr.attr_id and " +
+                "comm.object_id=Objects.object_id" +
+                " join attr deptno_attr on " +
+                "deptno_attr.object_type_id=object_types.object_type_id and " +
+                "deptno_attr.name='DEPTNO'" +
+                "join Params deptno on deptno.attr_id=deptno_attr.attr_id and " +
+                "deptno.object_id=objects.object_id " +
+//                "where \"Object_types\".\"name\"='people' and aid.\"number_value\"=11 and \n" +
+//                "apt.\"number_value\"=3;");
+
+                "WHERE object_types.OBJECT_TYPE_ID = 1511093759755 and objects.NAME = ?");
+        myStmt.setString(1, lastName);
+        rs = myStmt.executeQuery();
+
+//        while (rs.next()) {
+//            System.out.println(rs.getString("nam"));
+//            System.out.println(rs.getString("fname"));
+//            System.out.println(rs.getString("jobb"));
+//            System.out.println(rs.getDate("hiredatee"));
+//            System.out.println(rs.getInt("sall"));
+//            System.out.println(rs.getInt("commm"));
+//            System.out.println(rs.getLong("deptnoo"));
+//        }
+
+        while (rs.next()) {
+            employee = new Employee();
+            employee.setLastName(rs.getString("nam"));
+            employee.setFirstName(rs.getString("fname"));
+            employee.setJob(rs.getString("jobb"));
+            employee.setHiredate(rs.getDate("hiredatee"));
+            employee.setSalary(rs.getInt("sall"));
+            employee.setCommission(rs.getInt("commm"));
+            employee.setDeptNumber(rs.getLong("deptnoo"));
+            employees.add(employee);
+        }
+
+        return employees;
+    }
+
 
     public void close() throws SQLException {
         myConn.close();
